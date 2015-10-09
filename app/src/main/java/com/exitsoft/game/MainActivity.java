c6b6a08137e66d22c8122540d806815055936331
NaYunHwan <kbk9288@gmail.com>
2015-10-10 03:10:17 +0900

    Commit
    Commit

--- app/src/main/java/com/exitsoft/game/MainActivity.java
+++ app/src/main/java/com/exitsoft/game/MainActivity.java
@@ -7,37 +7,78 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+//    ImageButton btn1 = (ImageButton) findViewById(R.id.imageButton2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        ImageButton btn1 = (ImageButton) findViewById(R.id.imageButton2);
+//        btn1.setImageResource(R.drawable.char02);
+
+
+
 //        MainView
-        setContentView(new drawView(this));
+//        setContentView(new drawView(this));
     }
 
-    private class drawView extends View {
-        Paint mPaint;
-        Bitmap char01, char02, char03, char04;
-        drawView(Context context){
-            super(context);
-            char01 = BitmapFactory.decodeResource(getResources(),R.drawable.char01);
-            char02 = BitmapFactory.decodeResource(getResources(),R.drawable.char02);
-            char03 = BitmapFactory.decodeResource(getResources(),R.drawable.char03);
-            char04 = BitmapFactory.decodeResource(getResources(),R.drawable.char04);
-            mPaint = new Paint();
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        ImageButton btn1 = (ImageButton) findViewById(R.id.imageButton2);
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        int width = display.getWidth();
+        int height = display.getHeight();
+
+        int action = event.getAction();
+        if(action == MotionEvent.ACTION_DOWN){
+            if(event.getX() < width/2){
+//                Toast toast = Toast.makeText(this, "왼쪽쪽 입력.", Toast.LENGTH_SHORT);
+                btn1.setImageResource(R.drawable.char02);
+//                toast.show();
+            }
+            else{
+//                Toast toast = Toast.makeText(this, "오른쪽 입력.", Toast.LENGTH_SHORT);
+                btn1.setImageResource(R.drawable.char03);
+//                toast.show();
+            }
+
+
         }
 
-        protected void onDraw(Canvas canvas){
-            canvas.drawCircle(100,100,90,mPaint);
-            canvas.drawBitmap(char01,100,100,null);
-        }
+        return super.onTouchEvent(event);
     }
+
+//
+//    private class drawView extends View {
+//        Paint mPaint;
+//        Bitmap char01, char02, char03, char04;
+//        drawView(Context context){
+//            super(context);
+//            char01 = BitmapFactory.decodeResource(getResources(),R.drawable.char01);
+//            char02 = BitmapFactory.decodeResource(getResources(),R.drawable.char02);
+//            char03 = BitmapFactory.decodeResource(getResources(),R.drawable.char03);
+//            char04 = BitmapFactory.decodeResource(getResources(),R.drawable.char04);
+//            mPaint = new Paint();
+//        }
+//
+//        protected void onDraw(Canvas canvas){
+//            canvas.drawCircle(100,100,90,mPaint);
+//            canvas.drawBitmap(char01,100,100,null);
+//        }
+//    }
 //    WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
 //    2
 //    Display display = wm.getDefaultDisplay();
